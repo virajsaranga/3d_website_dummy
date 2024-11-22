a31d53a3860f71c430027424451e8f77ca6298af
virajsaranga <virajridi@gmail.com>
2024-11-22 22:38:20 +0530

    updated about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,8 +22,10 @@ const About = () => {
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Engineer based in Croatia, specializing in technical
-          education through hands-on learning and building applications.
+          Software Engineer 
+          
+         
+         
         </p>
       </div>
 
