93ed0b5822962f3812922821332efffd9ae037be
virajsaranga <virajridi@gmail.com>
2024-12-03 23:35:26 +0530

    update homeContain.js file

--- src/pages/homeContain.jsx
+++ src/pages/homeContain.jsx
@@ -8,7 +8,10 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
                     <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#testimonials">Testimonials</a></li>
+                    <li><a href="#blog">Blog</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
@@ -17,3 +20,4 @@ const Header = () => {
 };
 
 export default Header;
+
