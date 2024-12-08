8a97095cc72cce74434984a7769acf6acaeef849
virajsaranga <virajridi@gmail.com>
2024-12-08 22:33:48 +0530

    index.js file updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import Home from "./Home";
 import About from './About'
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Home from "./homeContain"
+
 
 export {
     Home,
     About,
     Projects,
-    Contact
+    Contact,
+    Home
 }
